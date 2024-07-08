517463303c7985be407f705496bc169cb846fbb5
grairudolf <153108176+grairudolf@users.noreply.github.com>
2024-07-08 08:18:18 +0100

    mail.php

--- mail.php
+++ mail.php
@@ -1,13 +1,13 @@
-<php
+<?php
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$to = "rtech777r@gmail.com";
+$to = "netboltz7@gmail.com";
 $subject = "Mail From NetBoltZ";
 $txt ="Name = ". $name . "\r\n Email = " . $email."\r\n message=" . $message.
 
-$header = "From: noreply@R_Tech.com" . "\r\n" .
+$header = "From: noreply@netboltz.netlify.app" . "\r\n" .
 "CC: somebodyelse@exampple.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
