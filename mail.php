104b5b3569c80d31ef8b1d5a067bfc0c7aaa5614
grairudolf <153108176+grairudolf@users.noreply.github.com>
2024-07-12 09:23:17 +0100

    mail.php

--- mail.php
+++ mail.php
@@ -3,15 +3,27 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$to = "netboltz7@gmail.com";
-$subject = "Mail From NetBoltZ";
-$txt ="Name = ". $name . "\r\n Email = " . $email."\r\n message=" . $message.
-
-$header = "From: noreply@netboltz.netlify.app" . "\r\n" .
-"CC: somebodyelse@exampple.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+if(!filter_var($email, FILTER_VALIDAT_EMAIL)){
+    echo "Invalid email address.";
+    exit;
 }
-//redirect
-header("location: index.html")
+//Recipient Email
+$to = "netboltz7@gmail.com";
+//Subject
+$subject = "Mail From NetBoltZ";
+//Email message
+$message_body = "Name:". $name . "\n"; 
+$message_body .= "Email:" . $email. "\n";
+$message_body .= "Message=" . $message;
+
+//Email headers
+$header = "From: " . $name  ."<" . $email . ">\r\n";
+$header = "Reply-To " . $email ."\r\n";
+//send email
+if(mail($to, $subject, $message_body, $header){
+       echo "Your message has successfully been sent";
+} else{
+    echo "Oops an error has occured. Please try again later";       
+}
+
 ?> 
